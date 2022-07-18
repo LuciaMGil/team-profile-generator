c17082ffabd284a7e7e783c88b3f8aab45c3dee4
Lucia Gil <luciagil5952@gmail.com>
2022-07-17 20:02:31 -0400

    Changed file name

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 
 // const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join('./', "team.html");
+const outputPath = path.join('./', "index.html");
 
 const render = require("./lib/htmlRenderer");
 
